6d78b257192defbcae7d63674a7e0f7ecdc13255
tunywalker <63519095+tunywalker@users.noreply.github.com>
2020-04-14 04:04:01 +0300

    MonitorKayitController oluşturuldu.

--- src/java/Controller/MonitorKayitController.java
+++ src/java/Controller/MonitorKayitController.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
-/**
- *
- * @author Bambi
- */
-public class MonitorKayitController {
+import Dao.MonitorKayitDAO;
+import Entity.MonitorKayit;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List; 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+
+public class MonitorKayitController implements Serializable {
+
+    private List<MonitorKayit> mklist;
+    private MonitorKayitDAO mkdao;
+    private MonitorKayit mk;
+
+    public List<MonitorKayit> getMklist() {
+        
+        if (mklist == null) {
+            this.mklist = new ArrayList<>();
+        }
+        this.mklist = this.getMkdao().getMonitorKayit();
+        return this.mklist;
+        
+    }
+
+    public void setMklist(List<MonitorKayit> mklist) {
+        this.mklist = mklist;
+    }
+
+    public MonitorKayitDAO getMkdao() {
+         if (mkdao == null) {
+            this.mkdao = new MonitorKayitDAO();
+        }
+        
+        return mkdao;
+    }
+
+    public void setMkdao(MonitorKayitDAO mkdao) {
+        this.mkdao = mkdao;
+    }
     
+
+   
+
 }
